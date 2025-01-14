226da6e8996ce2e1361ad02418a6a200f5086e1d
Nhut Tan <21130530@st.hcmuaf.edu.vn>
2025-01-14 23:51:59 +0700

    UPDATE: update new async await and update db connection function

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,11 +1,23 @@
-import express from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
+import {connectDb} from './utils/db-connector.js'
 
-const app = express();
+const app: Application = express();
 
 app.use(bodyParser.json());
 
-app.listen(process.env.DB_PORT, ()=>{
-    console.log(`Server started on port ${process.env.DB_PORT}`);
-});
+const startServer = async (): Promise<void> => {
+    try{
+        await connectDb();
+
+        app.listen(process.env.SERVER_PORT, ()=>{
+            console.log(`✅ Server khởi động ở cổng ${process.env.SERVER_PORT}`);
+        });
+    }catch(error){
+        console.error('❌ Lỗi khi khởi động ứng dụng:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
