e5c943fb775b1415d0ae050589e8a2995b7d8adf
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-02-03 18:21:00 +0700

    update: update config file for load infomation from env file

--- BackEnd/src/configs/env.ts
+++ BackEnd/src/configs/env.ts
@@ -6,12 +6,15 @@ config({
   example: join(process.cwd(), 'src', 'configs', '.env.example'),
   allowEmptyValues: true
 });
+console.log(process.env.DB_DIALECT,process.env.DB_HOST,
+  process.env.DB_PORT,process.env.DB_USER,
+  process.env.DB_PASSWORD,process.env.DB_NAME);
 export const env={
   DB_DIALECT: process.env.DB_DIALECT as 'mysql' | 'postgres' | 'sqlite' | 'mssql',
   DB_HOST: process.env.DB_HOST!,
   DB_PORT: Number(process.env.DB_PORT!),
   DB_USER: process.env.DB_USER!,
-  DB_PASS: process.env.DB_PASS!,
+  DB_PASSWORD: process.env.DB_PASSWORD!,
   DB_NAME: process.env.DB_NAME!,
   SERVER_PORT: Number(process.env.SERVER_PORT!),
   STATUS: process.env.STATUS!,
