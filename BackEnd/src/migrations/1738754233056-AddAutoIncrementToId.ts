a206732cbdf3b3166582cf355db767c9b9d999e4
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-02-06 23:37:49 +0700

    remove: remove unused migration file

--- BackEnd/src/migrations/1738754233056-AddAutoIncrementToId.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class AddAutoIncrementToId1738754233056 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    const table=['roles','users','products','categories', 'status']
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
