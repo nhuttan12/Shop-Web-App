b17e332d41d9f7594acb07bea81beb9c9aad4197
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-02-12 19:13:54 +0700

    refactor: change structure of schema

--- BackEnd/src/zod-schema/auth-schema/sign-up-shcema.ts
+++ BackEnd/src/zod-schema/auth-schema/sign-up-shcema.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 //create schema of user object for validation
 export const signUpSchema = z.object({
-  username: z.string().min(3, 'Username must be at least 5 characters'),
+  username: z.string().min(3, 'Username must be at least 3 characters'),
   email: z.string().email(),
   password: z.string().min(6, 'Password must be at least 4 characters'),
   retypePassword: z.string().min(6, 'Password must be at least 4 characters'),
