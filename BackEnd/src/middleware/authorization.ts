ece2cdb24c633665f8bc4f9468c1f0f79dd084ee
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-02-18 18:17:39 +0700

    refactor: seperate code for easy to maintence

--- BackEnd/src/middleware/authorization.ts
+++ BackEnd/src/middleware/authorization.ts
@@ -8,9 +8,10 @@ export function checkAbility(action: string, subject: string | object) {
   return (req: Request, res: Response, next: NextFunction) => {
     //get user information
     const user = req.user as User;
-    logger.debug(`Get user information ${user}`);
+    logger.debug(`Get user information ${JSON.stringify(user)}`);
 
     //check user information exists
+    logger.silly('Check user information exists');
     if (!user) {
       logger.debug('user not found');
       res.status(401).json({ error: messageLog.userIsnotAuthorized });
@@ -18,18 +19,21 @@ export function checkAbility(action: string, subject: string | object) {
     }
 
     //define ability for user
+    logger.silly('Define ability for user');
     const ability: AppAbility = defineAbilitiesFor(user);
-    logger.debug(`Define ability for user ${user.username}`);
+    logger.debug(`Define ability for user ${JSON.stringify(user.username)}`);
 
     //check user's ability to perform action on subject
+    logger.silly("Check user's ability to perform action on subject");
     if (ability.can(action, subject)) {
       logger.silly('user have permission');
       next();
     } else {
       logger.silly('user have no permission');
-      return res
+      res
         .status(403)
         .json({ error: messageLog.userIsNotAllowedToPerformAction });
+      return;
     }
   };
 }
--- BackEnd/src/routes/auth-route.ts
+++ BackEnd/src/routes/auth-route.ts
@@ -1,55 +1,38 @@
-import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response, Router } from 'express';
 
 import { SignUpController } from '../controllers/auth/sign-up-controller.js';
-import { signInSchema } from '../zod-schema/auth-schema/sign-in-schema.js';
-import passport from 'passport';
-import { env } from '../configs/env.js';
 import logger from '../utils/logger.js';
+import { signInSchema } from '../zod-schema/auth-schema/sign-in-schema.js';
+import { authenticateLocal } from '../middleware/auth-local-passport.js';
 import { messageLog } from '../utils/message-handling.js';
 
 const router = Router();
 
 router.post('/sign-up', SignUpController.signUp);
 
-router.post('/sign-in', (req: Request, res: Response, next: NextFunction) => {
-  //get data from request
-  const parsedData = signInSchema.safeParse(req.body);
-  logger.info(`Get data from reqeust ${req.body}`);
+router.post(
+  '/sign-in',
+  (req: Request, res: Response, next: NextFunction) => {
+    //get data from request
+    const parsedData = signInSchema.safeParse(req.body);
+    logger.info(`Get data from request ${req.body}`);
 
-  //checking data parsing process
-  if (!parsedData.success) {
-    return res.status(400).json({ error: parsedData.error.errors });
-  }
-  logger.info(`Checking data parsing process`);
-
-  return passport.authenticate(
-    'local',
-    { session: false },
-    (err: any, user: any, info: any) => {
-      //Error
-      if (err) {
-        logger.error(`Error: ${err.message}`);
-        return next(err);
-      }
-
-      //User not found
-      if (!user) {
-        logger.info(`Invalid credentials`);
-        return next(
-          new Error(info?.message || messageLog.invalidUsernameOrPassword)
-        );
-      }
-
-      //Generate token
-      logger.info(`Generate token`);
-      const token: string = jwt.sign({ id: user.id }, env.JWT_SECRET, {
-        expiresIn: '1h',
-      });
-
-      res.json({ message: messageLog.logInSuccessfully, token: token });
+    //checking data parsing process
+    logger.info(`Checking data parsing process`);
+    if (!parsedData.success) {
+      res.status(404).json({ error: parsedData.error.errors });
+      return;
     }
-  )(req, res, next);
-});
+    next();
+  },
+  authenticateLocal,
+  (req: Request, res: Response, next: NextFunction) => {
+    res.json({
+      message: messageLog.logInSuccessfully,
+      accessToken: res.locals.accessToken,
+      refreshToken: res.locals.refreshToken,
+    });
+  }
+);
 
 export { router };
