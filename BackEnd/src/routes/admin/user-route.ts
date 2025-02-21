f4e17eb5e46559c898343f9ceac03ada768318c0
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-02-21 21:44:15 +0700

    refactor: reuse middleware code

--- BackEnd/src/routes/admin/user-route.ts
+++ BackEnd/src/routes/admin/user-route.ts
@@ -1,27 +1,28 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import passport from 'passport';
 import { UserController } from '../../controllers/admin/users/user-controller.js';
 import { checkAbility } from '../../middleware/authorization.js';
+import { authenticateJwt } from '../../middleware/auth-jwt-passport.js';
 
 const router = Router();
 
 router.get(
   '/users',
-  passport.authenticate('jwt', { session: false }),
+  authenticateJwt,
   checkAbility('read', 'User'),
   UserController.getUsers
 );
 
 router.get(
   '/user/:id',
-  passport.authenticate('jwt', { session: false }),
+  authenticateJwt,
   checkAbility('read', 'User'),
   UserController.getUserById
 );
 
 router.put(
   '/user/:id',
-  passport.authenticate('jwt', { session: false }),
+  authenticateJwt,
   checkAbility('update', 'User'),
   UserController.updateUser
 );
