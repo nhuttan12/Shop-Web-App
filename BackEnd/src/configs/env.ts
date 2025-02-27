2c843b67ca2e9c1895ca3e0ad8921f0bc98a1246
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-02-27 19:44:03 +0700

    refactor: cmt for easy to maintain

--- BackEnd/src/configs/env.ts
+++ BackEnd/src/configs/env.ts
@@ -8,6 +8,7 @@ config({
 });
 
 export const env = {
+  //Database configuration
   DB_DIALECT: process.env.DB_DIALECT as
     | 'mysql'
     | 'postgres'
@@ -18,10 +19,17 @@ export const env = {
   DB_USER: process.env.DB_USER!,
   DB_PASSWORD: process.env.DB_PASSWORD!,
   DB_NAME: process.env.DB_NAME!,
+  //Application configuration
   SERVER_PORT: Number(process.env.SERVER_PORT!),
   STATUS: process.env.STATUS!,
+  //Jwt configuration
   JWT_SECRET: process.env.JWT_SECRET!,
   REFRESH_TOKEN_PRIVATE_KEY: process.env.REFRESH_TOKEN_PRIVATE_KEY!,
   EXPIRE_ACCESS_TOKEN_PRIVATE_KEY: process.env.EXPIRE_ACCESS_TOKEN_PRIVATE_KEY!,
   EXPIRE_REFRESH_TOKEN_PRIVATE_KEY: process.env.EXPIRE_REFRESH_TOKEN_PRIVATE_KEY!,
+  //Cloudinary configuration
+  CLOUD_NAME: process.env.CLOUD_NAME!,
+  SECURE: process.env.SECURE!,
+  API_KEY: process.env.API_KEY!,
+  API_SECRET: process.env.API_SECRET!,
 };
