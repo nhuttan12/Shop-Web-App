2f543ceb3f5dd53a9042b54b42b67164a581bb71
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-03-01 00:01:13 +0700

    update: adding new route for home

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn.tsx';
 import SignUp from './pages/SignUp.tsx';
+import Home from './pages/Home.tsx';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
+					<Route path='/' element={<Home />} />
 					<Route path='/sign-in' element={<SignIn />} />
 					<Route path='/sign-up' element={<SignUp />} />
 				</Routes>
