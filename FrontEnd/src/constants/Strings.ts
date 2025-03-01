7e3fedebc19d53591a25ca2fb4436d5cdfe30724
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-03-01 11:12:42 +0700

    refactor: refactor code

--- FrontEnd/src/constants/Strings.ts
+++ FrontEnd/src/constants/Strings.ts
@@ -1,9 +1,10 @@
-export const Strings={
-  username: 'Tài khoản',
-  password: 'Mật khẩu',
-  signIn: 'Đăng nhập',
-  forgotPassword: 'Quên mật khẩu?',
-  signUp: 'Đăng ký',
-  email: 'Email',
-  retypePassword: 'Nhập lại mật khẩu',
-}
+export const Strings = {
+	username: 'Tài khoản',
+	password: 'Mật khẩu',
+	signIn: 'Đăng nhập',
+	forgotPassword: 'Quên mật khẩu?',
+	signUp: 'Đăng ký',
+	email: 'Email',
+	retypePassword: 'Nhập lại mật khẩu',
+  haveAnAccount: 'Đã có tài khoản'
+};
--- FrontEnd/src/schemas/SignUpSchema.ts
+++ FrontEnd/src/schemas/SignUpSchema.ts
@@ -1,19 +1,21 @@
 import { errorMessage } from './../constants/ErrorMessage';
 import { z } from 'zod';
 
-export const signInSchema = z.object({
-  username: z.string().min(3, errorMessage.usernameHaveAtLeast3Character),
-  email: z.string().email(errorMessage.emailIsNotValid),
-  password: z.string().min(6, errorMessage.passwordHasAtLeast6Character),
-  retypePassword: z.string().min(6)
-}).superRefine((data, ctx)=>{
-  if(data.password!==data.retypePassword){
-    ctx.addIssue({
-      code: z.ZodIssueCode.custom,
-      message: errorMessage.retypePasswordHaveToSamePassword,
-      path: ['retypePassword'],
-    });
-  }
-});
+export const signUpSchema = z
+	.object({
+		username: z.string().min(3, errorMessage.usernameHaveAtLeast3Character),
+		email: z.string().email(errorMessage.emailIsNotValid),
+		password: z.string().min(6, errorMessage.passwordHasAtLeast6Character),
+		retypePassword: z.string().min(6),
+	})
+	.superRefine((data, ctx) => {
+		if (data.password !== data.retypePassword) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: errorMessage.retypePasswordHaveToSamePassword,
+				path: ['retypePassword'],
+			});
+		}
+	});
 
-export type signInType=z.infer<typeof signInSchema>;
+export type signUpType = z.infer<typeof signUpSchema>;
