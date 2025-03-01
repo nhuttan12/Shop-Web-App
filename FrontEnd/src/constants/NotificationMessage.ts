76b359e6a96ec3619350a7897e62484a028fbe2e
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-03-01 11:11:51 +0700

    update: remove winston logger library and  remove unnecessary message

--- FrontEnd/src/constants/NotificationMessage.ts
+++ FrontEnd/src/constants/NotificationMessage.ts
@@ -1,5 +1,2 @@
-export const notifyMessage={
-  pleaseFillInUsername: 'Vui lòng nhập tài khoản',
-  plaeseFillINPassword: 'Vui lòng nhập mật khẩu',
-  pleaseFillINEmail: 'Vui lòng nhập lại email',
-}
+export const notifyMessage = {
+};
