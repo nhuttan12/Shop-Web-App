5a8f704fcb077aa8d60df8550f488139e7cb8871
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-03-14 17:16:08 +0700

    refactor: refactor code for reduce warning

--- FrontEnd/src/pages/auth/SignUp.tsx
+++ FrontEnd/src/pages/auth/SignUp.tsx
@@ -1,11 +1,8 @@
 import SignUpForm from '../../components/auth/SignUpForm';
-import PageMeta from '../../components/common/PageMeta';
-import { Strings } from '../../constants/Strings';
 
 const SignUp = () => {
 	return (
 		<>
-			<PageMeta title={Strings.signUp} description={Strings.signUp} />
 			<SignUpForm />
 		</>
 	);
