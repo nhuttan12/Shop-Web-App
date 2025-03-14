62dfd24b966d5b37eacab9dc4bb532882e3173c4
NhutTan <phamnhuttan.9a6.2017@gmail.com>
2025-03-14 17:14:34 +0700

    update: update env file and config for loading attribute from env file

--- FrontEnd/src/configs/env.ts
+++ FrontEnd/src/configs/env.ts
@@ -1,15 +1,6 @@
-import { config } from 'dotenv-safe';
-import { join } from 'path';
-
-config({
-	path: join(process.cwd(), '.env'),
-	example: join(process.cwd(), '.env.example'),
-	allowEmptyValues: true,
-});
-
 export const env = {
 	//Application configuration
 	VITE_STATUS: import.meta.env.VITE_STATUS!,
 	VITE_SERVER_PORT: Number(import.meta.env.VITE_SERVER_PORT!),
-	VITE_SERVER_URL: import.meta.env.BACK_END_SERVER_URL!,
+	VITE_BACK_END_SERVER_URL: import.meta.env.VITE_BACK_END_SERVER_URL!,
 };
--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,32 +1,34 @@
-import { env } from './src/configs/env';
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr({ 
-      svgrOptions: {
-        // svgr options
-      },
-    }),
-  ],
-  define:{
-    'process.env':{}
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Thêm định nghĩa global để polyfill process nếu cần
-      define: {
-        global: 'globalThis'
+export default defineConfig(({ mode }) => {
+  // Tải biến môi trường dựa theo mode
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      svgr({ 
+        svgrOptions: {
+          // svgr options
+        },
+      }),
+    ],
+    define: {
+      'process.env': {}
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis'
+        }
       }
+    },
+    server: {
+      port: Number(env.VITE_SERVER_PORT)
     }
-  },
-  server: {
-    port: env.VITE_SERVER_PORT
   }
 })
